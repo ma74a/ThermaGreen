dea74a11d2433bcfeb463f67f74c9a717c0ddf74
mahmoud etman <metman835@gmail.com>
2026-08-14 12:32:56 +0300

    modified the path of the data

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -1,11 +1,15 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import torch
 from torch.utils.data import Dataset
 import torchvision.transforms as T
-
-from pathlib import Path
 from PIL import Image
+from pathlib import Path
 import numpy as np
 from typing import Tuple
+import matplotlib.pyplot as plt
 
 from utils.config import Config
 
@@ -15,17 +19,22 @@ class LoveDADataset(Dataset):
         self.split = split
         self.transforms = transforms
 
-        self.split_dir = self.data_root / self.split
+        self.split_dir = self.data_root / self.split / self.split
         self.data = []
 
         for domain in ["Rural", "Urban"]:
             image_dir = self.split_dir / domain / "images_png"
             masks_dir = self.split_dir / domain / "masks_png"
+
             for img_path in image_dir.glob("*.png"):
                 mask_path = masks_dir / img_path.name
 
                 if mask_path.exists():
-                    self.data.append((img_path, mask_path))
+                    self.data.append(
+                        (img_path, mask_path)
+                    )
+
+        print(f"{split}: {len(self.data)} samples")
 
     def __len__(self) -> int:
         return len(self.data)
@@ -42,3 +51,24 @@ class LoveDADataset(Dataset):
             mask = Config.TRANSFORMS_DICT["mask"](mask)
 
         return img, torch.tensor(np.array(mask), dtype=torch.long)
+
+
+# if __name__ == "__main__":
+#     obj = LoveDADataset(Config.DATA_ROOT, transforms=Config.TRANSFORMS_DICT)
+#     print(len(obj))
+#     img, mask = obj[0]
+#     print(img.shape)
+#     print(mask.shape)
+#     plt.figure(figsize=(10, 5))
+#     plt.subplot(1, 2, 1)
+#     plt.title("Image")
+#     plt.imshow(img.permute(1, 2, 0))
+#     plt.axis("off")
+
+#     plt.subplot(1, 2, 2)
+#     plt.title("Mask")
+#     plt.imshow(mask.cpu().numpy(), cmap="jet")
+#     plt.axis("off")
+
+#     plt.tight_layout()
+#     plt.show()
